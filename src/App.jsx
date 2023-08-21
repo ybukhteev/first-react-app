14b11066f942975477fdda28b49d65b6c8aa5a88
ybukhteev <mmm424@yandex.ru>
2023-08-21 12:20:27 +0300

    created the first react app, deleted main styles, add my style

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Simple HTML
+      <ul className="listing">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App;
